f3e79723defc45797dc9ec60357d2e877da61aeb
sayanthcp <sayanth646@gmail.com>
2023-10-12 14:33:07 +0530

    initial commit & navbar added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import NavBar from './components/NavBar'
+import {ThemeProvider} from './context/ThemeContext'
+
+function App() {
+
+  return (
+    <ThemeProvider>
+     <NavBar />
+    </ThemeProvider>
+  )
+}
+
+export default App
